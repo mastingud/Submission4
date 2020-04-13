af27f9fa37668e6296b93d1fb175f20bb0fff025
mastingud <dhio.reza93@gmail.com>
2020-04-13 22:27:34 +0700

    Create fungsi.js

---
+++ fungsi.js
@@ -0,0 +1,10 @@
+var readline = require('readline-sync');
+function kali(a, b) {
+return a * b;
+}
+var a = parseFloat(readline.question(' Masukan nilai a: '))
+var b = parseFloat(readline.question(' Masukan nilai b: '))
+var c;
+
+c = kali(a, b);
+console.log(` ${a} x ${b} = ${c} `);

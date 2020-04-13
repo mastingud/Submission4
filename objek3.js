551717f77555755e656828d0e09555659ba74955
mastingud <dhio.reza93@gmail.com>
2020-04-13 22:28:34 +0700

    Create objek3.js

---
+++ objek3.js
@@ -0,0 +1,15 @@
+var obj = {
+alas: 4, tinggi: 5, luas:function(){
+return this.alas * this.tinggi / 2;
+}
+}
+console.log(' Sebelum nilai properti diubah ');
+console.log(` obj.alas\t: ${obj.alas} `);
+console.log(` obj.tinggi\t: ${obj.tinggi} `);
+console.log(' obj.luas\t: ' +  obj.luas() );
+obj.alas = 10;
+obj.tinggi = 8;
+console.log(' \nSetelah nilai properti diubah ');
+console.log(` obj.alas\t: ${obj.alas} `);
+console.log(` obj.tinggi\t: ${obj.tinggi} `);
+console.log(' obj.luas\t: ' +  obj.luas() );
